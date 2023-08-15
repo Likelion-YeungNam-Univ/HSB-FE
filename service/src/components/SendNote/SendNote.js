d132cd734146b395acded88eb3fbcc7e74a98006
bagyerim <p0y0l0@naver.com>
2023-08-15 18:03:26 +0900

    쪽지 보내기 경고창 추가

--- service/src/components/SendNote/SendNote.js
+++ service/src/components/SendNote/SendNote.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import NoteSentConfirmation from "../SendNote/NoteSentConfirmation";
 
 const SendNoteContainer = styled.div`
   width: 100%;
@@ -22,7 +21,7 @@ const NoteForm = styled.form`
 
 const TextArea = styled.textarea`
  padding: 5px;
- width: 1160px;
+ width: 940px;
  height: 526px;
  border: 1px solid #BCBCBC;
 `;
@@ -33,7 +32,7 @@ const SendButton = styled.button`
   border: none;
   padding: 5px 10px;
   cursor: pointer;
-  align-self: flex-end; 
+  align-self: flex-start;
 `;
 
 const SearchContainer = styled.div`
@@ -43,8 +42,21 @@ const SearchContainer = styled.div`
   margin-bottom: 10px;
 `;
 
+const SearchButton = styled.button`
+  background-color: #95ddff;
+  color: #212121;
+  border: none;
+  padding: 5px 10px;
+  cursor: pointer;
+`;
+
 const SearchInput = styled.input`
   padding: 5px;
+`;
+
+const Divider = styled.div`
+  border-top: 1px solid #BCBCBC;
+  margin: 3px 0;
 `;
 
 const SendNote = () => {
@@ -54,12 +66,28 @@ const SendNote = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (receiver.trim() === "") {
+      window.alert("받는 사람을 입력해 주세요.");
+      return;
+    }
+
+    if (content.trim() === "") {
+      // 쪽지 내용이 비어있는 경우 경고 팝업창 표시
+      window.alert("쪽지 내용을 입력해주세요.");
+      return;
+    }
+
     // 쪽지 보내기 로직 구현
     console.log("Sending note to:", receiver);
     console.log("Content:", content);
 
-    // 보내기 완료 후 안내창 보이기
-    setShowConfirmation(true);
+    // 보내기 완료 후 안내 메시지를 경고창으로 표시
+    window.alert("쪽지를 성공적으로 보냈습니다.");
+
+    // 입력 필드 초기화
+    setReceiver("");
+    setContent("");
   };
 
   const handleSearchClick = () => {
@@ -81,22 +109,21 @@ const SendNote = () => {
           value={receiver}
           onChange={(e) => setReceiver(e.target.value)}
         />
-        <SendButton onClick={handleSearchClick}>검색</SendButton>
+        <SearchButton onClick={handleSearchClick}>검색</SearchButton>
       </SearchContainer>
       <NoteForm onSubmit={handleSubmit}>
+        <Divider />
+        <SendButton type="submit">보내기</SendButton>
+        <Divider />
         <ReceiverName>받는 사람: {receiver}</ReceiverName>
         <TextArea
           placeholder="말문희막힘"
           value={content}
           onChange={(e) => setContent(e.target.value)}
         />
-        <SendButton type="submit">보내기</SendButton>
       </NoteForm>
-      {showConfirmation && (
-        <NoteSentConfirmation onClose={handleCloseConfirmation} />
-      )}
     </SendNoteContainer>
-  );
+  )
 }
 
 export default SendNote;
