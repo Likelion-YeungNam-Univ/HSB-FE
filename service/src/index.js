3515f8fdbb0843e95187b3916248557c748176cc
yanni <122153297+yanni13@users.noreply.github.com>
2023-08-16 20:43:59 +0900

    fix:index.js

--- service/src/index.js
+++ service/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from "axios"
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/estimate"
+axios.defaults.baseURL = "http://127.0.0.1:8000/api"
 axios.defaults.withCredentials = true; //refreshToken cookie를 주고받기 위함
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
